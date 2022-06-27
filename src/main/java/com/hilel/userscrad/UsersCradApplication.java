16cad6f21cc5045295ccfd40399b653ab8065662
sos-alena <sev18121976@gmail.com>
2022-06-27 13:18:41 +0300

    start

---
+++ src/main/java/com/hilel/userscrad/UsersCradApplication.java
@@ -0,0 +1,13 @@
+package com.hilel.userscrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersCradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersCradApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hilel/userscrad/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.hilel.userscrad.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+@GetMapping("/users")
+    public String getUsers(){
+        return "users";
+    }
+}
